0a1de12b44c003fcf3c70d0300c11c81bc77da6c
Seemant11 <2068574@cognizant.com>
2022-02-15 18:56:15 +0530

    initial commit

---
+++ RailwayReservationSystem/RailwayReservationSystem/src/main/java/com/cts/RailwayReservationSystem/RailwayReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cts.RailwayReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayReservationSystemApplication.class, args);
+	}
+
+}
